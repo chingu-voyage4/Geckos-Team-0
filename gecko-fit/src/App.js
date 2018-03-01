eef9d12c91291a4ce417d6116d9297949b75a6fb
Shantia <shantiagrimes@gmail.com>
2018-02-28 23:20:45 -0600

    started styling elements using grid

--- gecko-fit/src/App.js
+++ gecko-fit/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App__header">
+          
+          <h1 className="App__title">Gecko Fit</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="ingredient-container">
+            <form className="ingredient-container__form">
+              <input type="text" name="ingredient" className="ingredient-container__field" placeholder="Type in ingredient to get nutrition info" /> 
+              <input type="submit" value="Search" className="ingredient-container__submit" />
+            </form>
+
+            <div>
+              <input type="button" value="Analyze Recipe" className="ingredient-container__analyze" /> 
+            </div>
+
+          </div>
+
       </div>
     );
   }
