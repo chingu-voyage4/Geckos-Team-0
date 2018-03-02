9543be39bff7964c0bd1c0eeb9839a9ca2c08412
Shantia <shantiagrimes@gmail.com>
2018-03-01 22:56:29 -0600

    completed basic ui html and css

--- gecko-fit/src/App.js
+++ gecko-fit/src/App.js
@@ -5,22 +5,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App__header">
-          
-          <h1 className="App__title">Gecko Fit</h1>
-        </header>
-          <div className="ingredient-container">
-            <form className="ingredient-container__form">
-              <input type="text" name="ingredient" className="ingredient-container__field" placeholder="Type in ingredient to get nutrition info" /> 
-              <input type="submit" value="Search" className="ingredient-container__submit" />
-            </form>
-
-            <div>
-              <input type="button" value="Analyze Recipe" className="ingredient-container__analyze" /> 
+        <div class="App__wrapper">
+          <header className="App__header">
+            
+            <h1 className="App__title">Gecko Fit</h1>
+          </header>
+            <div className="ingredient-container">
+              <form className="ingredient-container__form">
+                <input type="text" name="ingredient" className="ingredient-container__field" placeholder="Type in ingredient to get nutrition info" /> 
+                <input type="submit" value="Search" className="ingredient-container__submit" />
+              </form>
             </div>
-
+            <div className="ingredient-container__list">
+              
+            </div>
+            <div className="ingredient-container__analyze">
+              <input type="button" value="Analyze Recipe" /> 
+            </div>
           </div>
-
+          <footer className="footer">
+          <p className="footer__chingu-link">Created in <a href="#">Chingu Voyage-4</a></p>
+          <p className="footer__github-link"><a href="#">Link to Github</a></p>
+        </footer>
       </div>
     );
   }
