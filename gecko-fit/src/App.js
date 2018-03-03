9fc259ac09c41a49ce0fd552cc2032f40a037965
Shantia <shantiagrimes@gmail.com>
2018-03-03 00:55:19 -0600

    added banner and tweaked CSS

--- gecko-fit/src/App.js
+++ gecko-fit/src/App.js
@@ -24,8 +24,8 @@ class App extends Component {
             </div>
           </div>
           <footer className="footer">
-          <p className="footer__chingu-link">Created in <a href="#">Chingu Voyage-4</a></p>
-          <p className="footer__github-link"><a href="#">Link to Github</a></p>
+          <p className="footer__chingu-link">Created in <a href="https://chingu.io/">Chingu Voyage-4</a></p>
+          <p className="footer__github-link"><a href="https://github.com/chingu-voyage4/Geckos-Team-0">Link to Github</a></p>
         </footer>
       </div>
     );
