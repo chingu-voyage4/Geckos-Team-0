a984b1c3b24961f881919eae311025e89dd6db95
sdbowen <sdbowen.git@gmail.com>
2018-03-10 05:00:04 -0600

    handleInputChange added

--- gecko-fit/src/App.js
+++ gecko-fit/src/App.js
@@ -4,22 +4,58 @@ import "./App.css";
 import "react-select/dist/react-select.css";
 import Header from "./components/header";
 import SearchBar from "./components/searchBar";
-
 import Footer from "./components/footer";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autocomplete: "",
+      options: []
+    };
+  }
+
+  ingredientSearch(term) {
+    console.log("API search term: " + term);
+    fetch(
+      //change the last of this api call after to get different results ?q=
+      `https://cors-anywhere.herokuapp.com/http://api.edamam.com/auto-complete?q=${term}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          autocomplete: data
+        });
+        console.log("API return: " + this.state.autocomplete);
+      });
+  }
+  // format api results for react-select
+  apiResult() {
+    let arr = [];
+    let len = this.state.autocomplete.length;
+    for (var i = 0; i < len; i++) {
+      arr.push({
+        value: this.state.autocomplete[i],
+        label: this.state.autocomplete[i]
+      });
+    }
+    return arr;
+  }
   render() {
     return (
       <div className="App">
         <div className="App__wrapper">
           <Header />
           <div className="ingredient-wrapper">
-          <SearchBar />
-          <div className="ingredient-container__list" />>
-          <div className="ingredient-container__analyze">
-            <input type="button" value="Analyze Recipe" />
+            <SearchBar
+              onSearchTermChange={term => this.ingredientSearch(term)}
+            />
+            <div className="ingredient-container__list" />
+            <div className="ingredient-container__analyze">
+              <input type="button" value="Analyze Recipe" />
+            </div>
           </div>
-        </div>
         </div>
         <Footer />
       </div>
--- gecko-fit/src/components/searchBar.js
+++ gecko-fit/src/components/searchBar.js
@@ -1,73 +1,49 @@
 import React, { Component } from "react";
 import Select from "react-select";
 
-
 class SearchBar extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        selectedOption: "",
-        autocomplete: "",
-        options: []
-      };
-    }
-    componentDidMount() {
-      fetch(
-        //change the last of this api call after to get different results ?q=
-        `https://cors-anywhere.herokuapp.com/http://api.edamam.com/auto-complete?q=ap`
-      )
-        .then(res => res.json())
-        .then(data => {
-          this.setState({
-            autocomplete: data
-          });
-          console.log(this.state.autocomplete);
-          console.log(this.apiResult());
-        });
-    }
-    // format api results for react-select
-    apiResult() {
-      let arr = [];
-      let len = this.state.autocomplete.length;
-      for (var i = 0; i < len; i++) {
-        arr.push({
-          value: this.state.autocomplete[i],
-          label: this.state.autocomplete[i]
-        });
-      }
-      return arr;
-    }
-    // Display user search input
-    handleInputChange = selectedOption => {
-      this.setState({ selectedOption });
-      // For testing
-      if (selectedOption) {
-        console.log(`Selected: ${this.state.selectedOption}`);
-      }
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {
+      selectedOption: "",
     };
-  
-    render() {
-      return (
-          <div className="ingredient-container">
-            <form className="ingredient-container__form">
-            <Select
-                type="text"
-                name="ingredient"
-                className="ingredient-container__field"
-                placeholder="Type in ingredient to get nutrition info"
-                value={this.state.value}
-                onChange={this.handleInputChange}
-                options={this.apiResult()}
-              />
-              <input
-                type="submit"
-                value="Add"
-                className="ingredient-container__submit"
-              />
-            </form>
-          </div>
-      );
-    }
   }
 
-  export default SearchBar;
+  render() {
+    return (
+      <div className="ingredient-container">
+        <form className="ingredient-container__form">
+          <Select
+            type="text"
+            name="ingredient"
+            className="ingredient-container__field"
+            placeholder="Type in ingredient to get nutrition info"
+            //value={this.state.term}
+            onChange={this.handleOptionSelect}
+            onInputChange={this.handleInputChange}
+            //options={this.props.options}
+          />
+          <input
+            type="submit"
+            value="Add"
+            className="ingredient-container__submit"
+          />
+        </form>
+      </div>
+    );
+  }
+
+  // Display user search input
+  handleInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+    return term;
+  }
+  handleOptionSelect(selectedOption) {
+    this.setState({ selectedOption });
+    console.log(`Selected: ${selectedOption.label}`);
+  }
+}
+
+export default SearchBar;
