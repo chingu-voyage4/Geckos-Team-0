9b372c3ee8419379938981bc23cc950832b02713
abecel22 <abe2211@gmail.com>
2018-03-13 08:40:49 -0500

    Added nutrition call from api. Results of data in console.

--- gecko-fit/src/App.js
+++ gecko-fit/src/App.js
@@ -1,106 +1,123 @@
-import React, { Component } from "react";
-import Select from "react-select";
-import "./App.css";
-import "react-select/dist/react-select.css";
-import Header from "./components/header";
-import SearchBar from "./components/searchBar";
-import Ingredient from "./components/ingredient";
-import Footer from "./components/footer";
+import React, { Component } from 'react';
+import Select from 'react-select';
+import './App.css';
+import 'react-select/dist/react-select.css';
+import Header from './components/header';
+import SearchBar from './components/searchBar';
+import Ingredient from './components/ingredient';
+import Footer from './components/footer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.addIngredient = this.addIngredient.bind(this);
-    this.removeIngredient = this.removeIngredient.bind(this);
-    this.state = {
-      autocomplete: "",
-      ingredients: []
-    };
-  }
-  // add ingredient to ingredient list 
-  addIngredient (ingredient) {
-    if(!ingredient) {
-      return 'Enter an ingredient';
+    constructor(props) {
+        super(props);
+        this.addIngredient = this.addIngredient.bind(this);
+        this.removeIngredient = this.removeIngredient.bind(this);
+        this.state = {
+            autocomplete: '',
+            ingredients: []
+        };
     }
-    
-    this.setState((prevState) =>{
-      return {
-        ingredients: prevState.ingredients.concat(ingredient)
-      };
-    }); 
-      console.log(this.state.ingredients);
-  }
-  // remove ingredient from ingredient list
-  removeIngredient (ingredient) {
-    this.setState ((prevState) => {
-      return {
-        ingredients: prevState.ingredients.filter(element => element !== ingredient)
-      };
-    });
-      
-    console.log(`From removeIngredient: ${ingredient}`);
+    // add ingredient to ingredient list
+    addIngredient(ingredient) {
+        if (!ingredient) {
+            return 'Enter an ingredient';
+        }
 
-  }
-  
-  ingredientSearch(term) {
-    fetch(
-      //change the last of this api call after to get different results ?q=
-      `https://cors-anywhere.herokuapp.com/http://api.edamam.com/auto-complete?q=${term}`
-      
-    )
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          autocomplete: data
+        this.setState((prevState) => {
+            return {
+                ingredients: prevState.ingredients.concat(ingredient)
+            };
         });
-        this.apiResult();
-      });
-  } 
-
-  // format api results for react-select
-  apiResult() {
-    let searchResult = [];
-    let len = this.state.autocomplete.length;
-    for (var i = 0; i < len; i++) {
-      searchResult.push({
-        value: this.state.autocomplete[i],
-        label: this.state.autocomplete[i]
-      });
+        console.log(this.state.ingredients);
     }
-    return searchResult;
-  }
-  // fetch selected ingredient from searchBar
-  ingredientSelection(item) {
-    console.log("Selected ingredient:");
-    console.log(item);
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App__wrapper">
-          <Header />
-          <div className="ingredient-wrapper">
-            <SearchBar
-              onSearchTermChange={term => this.ingredientSearch(term)}
-              searchResult={this.apiResult()}
-              ingredientSelection={this.ingredientSelection}
-              addIngredient={this.addIngredient}
-              ingredients={this.state.ingredients}
-            />
-            <div className="ingredient-container__list">
-              {
-                this.state.ingredients.map((ingredient) => <Ingredient key={ingredient} ingredientText={ingredient} removeIngredient={this.removeIngredient}/>)
-              }
+    // remove ingredient from ingredient list
+    removeIngredient(ingredient) {
+        this.setState((prevState) => {
+            return {
+                ingredients: prevState.ingredients.filter(
+                    (element) => element !== ingredient
+                )
+            };
+        });
+
+        console.log(`From removeIngredient: ${ingredient}`);
+    }
+
+    ingredientSearch(term) {
+        fetch(
+            //change the last of this api call after to get different results ?q=
+            `https://cors-anywhere.herokuapp.com/http://api.edamam.com/auto-complete?q=${term}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    autocomplete: data
+                });
+                this.apiResult();
+            });
+    }
+
+    // format api results for react-select
+    apiResult() {
+        let searchResult = [];
+        let len = this.state.autocomplete.length;
+        for (var i = 0; i < len; i++) {
+            searchResult.push({
+                value: this.state.autocomplete[i],
+                label: this.state.autocomplete[i]
+            });
+        }
+        return searchResult;
+    }
+    // fetch selected ingredient from searchBar
+    ingredientSelection(item) {
+        console.log('Selected ingredient:' + item.label);
+        const ingredientName = item.label;
+        const app_id = '20f484de';
+        const app_key = '58758eaee27454caa774cc54092a9fc6';
+        fetch(
+            //this api call needs quantity , unit , and ingredient.
+            //Have a space between each.
+            `https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&ingr=4 oz ${ingredientName} `
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App__wrapper">
+                    <Header />
+                    <div className="ingredient-wrapper">
+                        <SearchBar
+                            onSearchTermChange={(term) =>
+                                this.ingredientSearch(term)
+                            }
+                            searchResult={this.apiResult()}
+                            ingredientSelection={this.ingredientSelection}
+                            addIngredient={this.addIngredient}
+                            ingredients={this.state.ingredients}
+                        />
+                        <div className="ingredient-container__list">
+                            {this.state.ingredients.map((ingredient) => (
+                                <Ingredient
+                                    key={ingredient}
+                                    ingredientText={ingredient}
+                                    removeIngredient={this.removeIngredient}
+                                />
+                            ))}
+                        </div>
+                        <div className="ingredient-container__analyze">
+                            <input type="button" value="Analyze Recipe" />
+                        </div>
+                    </div>
+                </div>
+                <Footer />
             </div>
-            <div className="ingredient-container__analyze">
-              <input type="button" value="Analyze Recipe" />
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
