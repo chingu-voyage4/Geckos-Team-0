3fd97afaa15d8e0ddc16e476471b5fdc29ccb49d
abecel22 <abe2211@gmail.com>
2018-03-24 23:20:37 -0500

    Added error handling to api calls . Blocks misspellings and duplicates.

--- gecko-fit/src/components/optionModal.js
+++ gecko-fit/src/components/optionModal.js
@@ -2,8 +2,14 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-    <Modal isOpen={props.error} contentLabel="Try a different value">
-        <h3>Try a Different Value</h3>
+    <Modal
+        isOpen={!!props.error}
+        onRequestClose={props.handleClearErrors}
+        contentLabel="Try a different value"
+        ariaHideApp={false}
+    >
+        <h3>{props.error}</h3>
+        <button onClick={props.handleClearErrors}>Close</button>
     </Modal>
 );
 
