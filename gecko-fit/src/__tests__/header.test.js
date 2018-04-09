66b5bb31434952d88a649b4c15853557638f9254
Shantia Grimes <shantiagrimes@gmail.com>
2018-04-09 09:48:15 -0500

    revised tests using enzyme

--- gecko-fit/src/__tests__/header.test.js
+++ gecko-fit/src/__tests__/header.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow, render} from 'enzyme';
 import Header from '../components/header';
 
-test ('Should render header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+jest.useFakeTimers();
+// test Header render()
+describe('Header Component', () => {
+  it('Should render header correctly', () => {
+    const wrapper = shallow(<Header />);
+    const header = {};
+
+    expect(
+      wrapper.exists(
+        <header className="App__header" style={header}>
+        </header>
+      )
+    ).toBe(true);
+  });
+  // test componentDidMount calling setInterval and setInterval calling randomImg
+  it('timer that will call randomImg function fires', () => {
+    const wrapper = shallow(<Header />);
+    // creates a spy to spy on Header prototype randomImg
+    const spy = jest.spyOn(Header.prototype, 'randomImg');
+    // creates instance of randomImg
+    wrapper.instance().randomImg();
+    expect(setInterval).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
+    
+  });
 });
