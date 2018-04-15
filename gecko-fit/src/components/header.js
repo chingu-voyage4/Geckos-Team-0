68cd98a33fc686ecf0e3f716331594c8eacaefff
abecel22 <abe2211@gmail.com>
2018-04-15 17:47:10 -0500

    Improved background image responsiveness for smaller screens.

--- gecko-fit/src/components/header.js
+++ gecko-fit/src/components/header.js
@@ -1,41 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    const defaultImg = require('../img/Food_00.jpg');
-    const logo = require('../img/Gecko_fit_logo.png');
-    this.state = {
-      imgUrl:  defaultImg,
-      logo: logo
+    constructor(props) {
+        super(props);
+        const defaultImg = require('../img/Food_00.jpg');
+        const logo = require('../img/Gecko_fit_logo.png');
+        this.state = {
+            imgUrl: defaultImg,
+            logo: logo
+        };
     }
-  }
-  randomImg() {
-    const randomNum = Math.floor(Math.random() * 3);
-    const imgUrl = require('../img/Food_0' + randomNum + '.jpg');
-    this.setState(() => {
-      return {
-        imgUrl: imgUrl
-      };
-    });
-  }
-  componentDidMount() {
-    // change to 3000 if you want to see it in action
-    setInterval(() => this.randomImg(), 120000); 
-  }
+    randomImg() {
+        const randomNum = Math.floor(Math.random() * 3);
+        const imgUrl = require('../img/Food_0' + randomNum + '.jpg');
+        this.setState(() => {
+            return {
+                imgUrl: imgUrl
+            };
+        });
+    }
+    componentDidMount() {
+        // change to 3000 if you want to see it in action
+        setInterval(() => this.randomImg(), 120000);
+    }
     render() {
-      const header = {
-        backgroundColor: 'black',
-        backgroundImage: 'url("' + this.state.imgUrl + '")',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
-      };
-      return (
-        <header className="App__header" style={header}>
-          <img src={this.state.logo} alt='Gecko Fit logo' height='100' width='100' />
-        </header>
-      );
+        const header = {
+            backgroundColor: 'black',
+            backgroundImage: 'url("' + this.state.imgUrl + '")',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'
+        };
+        return (
+            <header className="App__header" style={header}>
+                <img
+                    src={this.state.logo}
+                    alt="Gecko Fit logo"
+                    height="100"
+                    width="100"
+                />
+            </header>
+        );
     }
-  }
+}
 
 export default Header;
