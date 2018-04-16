b7ca67082778125c6400ce936fe7e2fc5e615d5a
Shantia Grimes <shantiagrimes@gmail.com>
2018-04-15 20:51:49 -0500

    changed footer links to icons and footer height

--- gecko-fit/src/components/footer.js
+++ gecko-fit/src/components/footer.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    const githubLogo = require('../img/GitHub-Mark-32px.png');
+    const chinguLogo = require('../img/chingu_logo.png');
+    this.state = {
+      chingu: chinguLogo,
+      github: githubLogo
+    }
+  }
   render() {
     return (
       <footer className="footer">
-        <p className="footer__chingu-link">
-          Created in <a href="https://chingu.io/">Chingu Voyage-4</a>
-        </p>
         <p id="edamam-badge" data-color="transparent" className="footer__edamam-link"></p>
+        <p className="footer__chingu-link">
+        <a href="https://chingu.io/">
+          <img 
+            src={this.state.chingu}
+            alt="Chingu logo"
+            height="32"
+            width="32"
+          /></a>
+        </p>
         <p className="footer__github-link">
           <a href="https://github.com/chingu-voyage4/Geckos-Team-0">
-            Link to Github
+            <img 
+              src={this.state.github}
+              alt="Github logo"
+              height="32"
+              width="32"
+            />
           </a>
         </p>
       </footer>
